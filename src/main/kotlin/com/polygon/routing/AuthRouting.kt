0331799d320e1785b7958e6a290cbad9cb1c7bc8
iamomidk <iamomidk@gmail.com>
2023-07-10 17:27:40 +0330

    -configured routing

---
+++ src/main/kotlin/com/polygon/routing/AuthRouting.kt
@@ -0,0 +1,54 @@
+package com.polygon.routing
+
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
+import com.polygon.model.User
+import com.polygon.services.UserService
+import com.polygon.utils.jwtAudience
+import com.polygon.utils.jwtExpiration
+import com.polygon.utils.jwtIssuer
+import com.polygon.utils.jwtSecret
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.config.*
+import io.ktor.server.engine.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.buildJsonObject
+import kotlinx.serialization.json.encodeToJsonElement
+import kotlinx.serialization.json.put
+import java.util.*
+
+fun Route.authRouting(config: ApplicationConfig, userService: UserService) {
+	post("/login") {
+		val user = call.receive<User>()
+		val userStatus = userService.loginUser(user.username, user.password)
+		if (userStatus) {
+			call.respond(
+				buildJsonObject {
+					put("description", HttpStatusCode.OK.description)
+					put("result", Json.encodeToJsonElement(userService.userRepository.findByUsername(user.username)))
+				}
+			)
+		} else call.respond(HttpStatusCode.Unauthorized, "username/password is wrong")
+	}
+	
+	
+	post("/signup") {
+		try {
+			val user = call.receive<User>()
+			val token = JWT.create()
+				.withAudience(config.jwtAudience)
+				.withIssuer(config.jwtIssuer)
+				.withExpiresAt(Date(System.currentTimeMillis() + config.jwtExpiration))
+				.sign(Algorithm.HMAC256(config.jwtSecret))
+			user.token = token
+			userService.createUser(user)
+			call.respond(HttpStatusCode.Created, "User singed up")
+		} catch (e: Exception) {
+			logError(call, e)
+		}
+	}
+}
---
+++ src/main/kotlin/com/polygon/routing/UsersRouting.kt
@@ -0,0 +1,76 @@
+package com.polygon.routing
+
+import com.polygon.model.User
+import com.polygon.services.UserService
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.buildJsonObject
+import kotlinx.serialization.json.encodeToJsonElement
+import kotlinx.serialization.json.put
+
+fun Route.usersRouting(userService: UserService) {
+	route("/user") {
+		get {
+			try {
+				val token = call.request.headers["Authorization"] ?: return@get call.respond(
+					status = HttpStatusCode.BadRequest, hashMapOf("description" to "Authorization header not found")
+				)
+				val user = userService.userRepository.findByToken(token) ?: return@get call.respond(
+					status = HttpStatusCode.NotFound, hashMapOf("description" to "No User Found")
+				)
+				return@get if (userService.userRepository.authorized(token)) {
+					call.respond(buildJsonObject {
+						put("description", HttpStatusCode.OK.description)
+						put("result", Json.encodeToJsonElement(user))
+					})
+				} else call.respond(
+					status = HttpStatusCode.Unauthorized, hashMapOf("description" to HttpStatusCode.Unauthorized.description)
+				)
+			} catch (e: Exception) {
+				call.application.environment.log.error(e.toString())
+			}
+		}
+		get("/{id?}") {
+			try {
+				val token = call.request.headers["Authorization"] ?: return@get call.respond(
+					status = HttpStatusCode.BadRequest, hashMapOf("description" to "Authorization header not found")
+				)
+				val id = call.parameters["id"]?.toLongOrNull() ?: return@get call.respond(
+					status = HttpStatusCode.NotFound, hashMapOf("description" to "Missing id")
+				)
+				val user = userService.userRepository.findById(id) ?: return@get call.respond(
+					status = HttpStatusCode.NotFound, hashMapOf("description" to "No User with id: $id")
+				)
+				return@get if (userService.userRepository.authorized(token)) {
+					call.respond(buildJsonObject {
+						put("description", HttpStatusCode.OK.description)
+						put("result", Json.encodeToJsonElement(user))
+					})
+				} else call.respond(
+					status = HttpStatusCode.Unauthorized, hashMapOf("description" to HttpStatusCode.Unauthorized.description)
+				)
+			} catch (e: Exception) {
+				call.application.environment.log.error(e.toString())
+			}
+		}
+		post {
+			val token = call.request.headers["Authorization"] ?: return@post call.respond(
+				status = HttpStatusCode.BadRequest, hashMapOf("description" to "Authorization header not found")
+			)
+			return@post if (userService.userRepository.authorized(token)) {
+				val user = call.receive<User>()
+				userService.userRepository.updateUser(token, user)
+				call.respond(buildJsonObject {
+					put("description", HttpStatusCode.OK.description)
+					put("result", Json.encodeToJsonElement(userService.userRepository.findByToken(token)))
+				})
+			} else call.respond(
+				status = HttpStatusCode.Unauthorized, hashMapOf("description" to HttpStatusCode.Unauthorized.description)
+			)
+		}
+	}
+}
